e259714707eb180ad314077a2cd4a7c8d850c3f6
Miguel Mateo Grajales <matius404@gmail.com>
2023-05-06 21:18:46 -0500

    Creación de datasource, modelos, controladores, repositorios y relaciones

---
+++ src/config/general-config.ts
@@ -0,0 +1,10 @@
+import path from "path";
+
+export namespace GeneralConfig {
+    //export const carpetaArchivosProductos: string = '../archivos/productos';
+    export const carpetaArchivosProductos: string = "archivos/productos";
+    export const carpetaArchivosClientes: string = "archivos/clientes";
+    export const campoDeProducto: string = "file";
+    export const extensionesImagenes: string[] = [".JPG", ".PNG", ".JPEG"];
+
+}
